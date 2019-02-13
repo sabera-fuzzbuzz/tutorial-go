73dc261101ea3b9718682477390d9ebc27ce7104
Everest Munro-Zeisberger <evmunro1@gmail.com>
2019-02-13 11:15:28 -0800

    :rocket:

--- fuzz.go
+++ fuzz.go
@@ -3,6 +3,8 @@ package tutorial
 // FuzzerEntrypoint is the method Fuzzbuzz will repeatedly
 // run with new tests to try and find bugs in BrokenMethod
 func FuzzerEntrypoint(Data []byte) int {
-	BrokenMethod(Data)
+	// Convert Data to the type we need:
+	testString := string(Data)
+	BrokenMethod(testString)
 	return 0
 }
--- method.go
+++ method.go
@@ -2,7 +2,7 @@ package tutorial
 
 // BrokenMethod has a bug - it will try to read the 4th
 // index of Data even when it only has a length of 3.
-func BrokenMethod(Data []byte) bool {
+func BrokenMethod(Data string) bool {
 	return len(Data) >= 3 &&
 		Data[0] == 'F' &&
 		Data[1] == 'U' &&
